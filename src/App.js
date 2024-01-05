1a04fa1457a4a1636e9633a14ac9a208e9303e1b
kingkaminseo <136690999+kingkaminseo@users.noreply.github.com>
2024-01-05 17:15:24 +0900

    counter 앱 제작 (react)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState(1);
+
+  const onChange2 = (ee) => {
+    setCount(Number(ee.target.value));
+  };
+
+  const onChange = (e) => {
+    setNewCount(Number(e.target.value));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h1>{count}</h1>
+        <div>
+          <button onClick={() => setCount(count - newCount)}>Down</button>
+          <button onClick={() => setCount(count + newCount)}>Up</button>
+        </div>
+
+      <div className='flexbox'>
+        <div>
+          <input type='number' placeholder='시작숫자' onChange={onChange2}></input>
+        </div>
+        <div>
+          <input type='number' placeholder='단위' onChange={onChange}></input>
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default App;
